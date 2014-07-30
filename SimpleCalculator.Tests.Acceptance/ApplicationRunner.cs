3c3ff28dc9569f4e0180c278e6c46ac821dc0b6f
Jaimen.Lathia <JaimenLathia@gmail.com>
2014-07-31 00:09:42 +0100

    Created initial end to end test project

---
+++ SimpleCalculator.Tests.Acceptance/ApplicationRunner.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace SimpleCalculator.Tests.Acceptance
+{
+    class ApplicationRunner
+    {
+        Process _process;
+
+        public ApplicationRunner()
+        {
+            _process = new Process();
+
+            _process.StartInfo.FileName = "SimpleCalculator.Application.exe";
+            _process.StartInfo.UseShellExecute = false;
+            _process.StartInfo.RedirectStandardOutput = true;
+            _process.StartInfo.RedirectStandardInput = true;
+
+            _process.Start();
+        }
+
+        internal void Displays(string expectedText)
+        {
+            Assert.AreEqual(expectedText, _process.StandardOutput.ReadLine());
+        }
+
+        internal void UserInputs(string inputText)
+        {
+            _process.StandardInput.WriteLine(inputText);
+        }
+    }
+}
---
+++ SimpleCalculator.Tests.Acceptance/CalculatorEndToEndTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator.Tests.Acceptance
+{
+    [TestFixture]
+    public class CalculatorEndToEndTests
+    {
+        [TestCase]
+        public void CalculatorAddsTwoNumbersTogether()
+        {
+            ApplicationRunner application = new ApplicationRunner();
+
+            application.Displays("Enter a number : ");
+            application.UserInputs("1");
+            application.Displays("Enter a number : ");
+            application.UserInputs("1");
+            application.Displays("Enter an operator : ");
+            application.UserInputs("+");
+            application.Displays("Result = 2");
+        }
+    }
+}
