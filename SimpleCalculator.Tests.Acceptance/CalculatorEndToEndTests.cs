930501ee53bbb49313e42da7ababe652df25d5db
Jaimen.Lathia <JaimenLathia@gmail.com>
2014-08-26 13:50:47 +0100

    Refactored Acceptance tests to use setup fixture for creating the Application Runner

--- SimpleCalculator.Tests.Acceptance/CalculatorEndToEndTests.cs
+++ SimpleCalculator.Tests.Acceptance/CalculatorEndToEndTests.cs
@@ -10,64 +10,62 @@ namespace SimpleCalculator.Tests.Acceptance
     [TestFixture]
     public class CalculatorEndToEndTests
     {
+        private ApplicationRunner _application;
+
+        [SetUp]
+        public void Setup()
+        {
+            _application = new ApplicationRunner();
+        }
+
         [Test]
         public void CalculatorAddsTwoNumbersTogether()
         {
-            ApplicationRunner application = new ApplicationRunner();
-
-            application.Displays("Enter a number : ");
-            application.UserInputs("1");
-            application.Displays("Enter a number : ");
-            application.UserInputs("1");
-            application.Displays("Enter an operator : ");
-            application.UserInputs("+");
-            application.Displays("Result = 2");
+            _application.Displays("Enter a number : ");
+            _application.UserInputs("1");
+            _application.Displays("Enter a number : ");
+            _application.UserInputs("1");
+            _application.Displays("Enter an operator : ");
+            _application.UserInputs("+");
+            _application.Displays("Result = 2");
         }
 
         [Test]
         public void CalculatorValidatesOutOfBoundsIntergersForFirstNumber()
         {
-            ApplicationRunner application = new ApplicationRunner();
-
-            application.Displays("Enter a number : ");
-            application.UserInputs("99999999999");
-            application.DisplayContains("Error");
+            _application.Displays("Enter a number : ");
+            _application.UserInputs("99999999999");
+            _application.DisplayContains("Error");
         }
 
         [Test]
         public void CalculatorValidatesOutOfBoundsIntergersForSecondNumber()
         {
-            ApplicationRunner application = new ApplicationRunner();
-
-            application.Displays("Enter a number : ");
-            application.UserInputs("1");
-            application.Displays("Enter a number : ");
-            application.UserInputs("99999999999");
-            application.DisplayContains("Error");
+            _application.Displays("Enter a number : ");
+            _application.UserInputs("1");
+            _application.Displays("Enter a number : ");
+            _application.UserInputs("99999999999");
+            _application.DisplayContains("Error");
         }
 
         [Test]
         public void CalculatorValidatesNonNumericCharactersForIntergers()
         {
-            ApplicationRunner application = new ApplicationRunner();
-
-            application.Displays("Enter a number : ");
-            application.UserInputs("a");
-            application.DisplayContains("Error");
+            _application.Displays("Enter a number : ");
+            _application.UserInputs("a");
+            _application.DisplayContains("Error");
         }
 
         [Test]
         public void CalculatorValidatesOperatorIsKnownCharacter()
         {
-            ApplicationRunner application = new ApplicationRunner();
-
-            application.Displays("Enter a number : ");
-            application.UserInputs("1");
-            application.Displays("Enter a number : ");
-            application.UserInputs("1");
-            application.Displays("Enter an operator : ");
-            application.UserInputs("a");
-            application.DisplayContains("Error");
+            _application.Displays("Enter a number : ");
+            _application.UserInputs("1");
+            _application.Displays("Enter a number : ");
+            _application.UserInputs("1");
+            _application.Displays("Enter an operator : ");
+            _application.UserInputs("a");
+            _application.DisplayContains("Error");
         }
     }
 }
